e6bcc5621d992c64b97abdd228ecd79334ec4b62
Carlos <cs515164@gmail.com>
2020-09-06 14:56:19 -0300

    Project 002 upload

--- Day 002 - 2019 Mood Calendar/script.js
+++ Day 002 - 2019 Mood Calendar/script.js
@@ -1,16 +1,33 @@
-teste = document.getElementById("teste")
-teste_btt = document.getElementById("testeBtt")
+let content = document.getElementById("content")
 
-function sla() {
+function createCalendar() {
     function getDayNumbers(month) {
         month_days = []
         monthCount++
 
-        console.log(month)
         if(months[monthCount-1] === month) {
+
+            for(let i=0; i<week_days.length; i++) {
+
+                if(week_days[next_week_day-1] === week_days[i]) {
+
+                    for(let c=1; c<=i; c++) {
+                        month_days.push("")
+                    }
+
+                }
+
+            }
+
             for(let i=1; i<=days_in_months[monthCount-1]; i++) {
                 month_days.push(i)
+
+                if(next_week_day > 5) helper_week_day = week_days[0]
+                else helper_week_day = week_days[next_week_day+1]
+
+                next_week_day = week_days.indexOf(helper_week_day)
             }
+
             return month_days
         }
     }
@@ -21,8 +38,23 @@ function sla() {
 
     let monthCount = 0
     let month_days = []
+    let helper_week_day = week_days[2]
+    let next_week_day = week_days.indexOf(helper_week_day) + 1
 
     const newInnerHTML = `
+        <div id="moodGroup">
+            <h2>Calendário de Ânimo 2019</h2>
+            <h4 id="instruct">~ pinte cada dia dependendo de qual era / é seu ânimo ~</h4>
+            <div id="moodSelection">
+                <p>Selecione seu ânimo:</p>
+                <div class="moodContainer">
+                    ${moods.map(mood => `
+                    <button class="mood" id="mood${moods.indexOf(mood)+1}">${mood}</button>
+                    `).join("")}
+                </div>
+                <p>depois clique nos círculos abaixo.</p>
+            </div>
+        </div>
         <div id="calendar">
             <div class="monthsGroup">
                 ${months.map(month => `<div class="month"><h3>${month}</h3>
@@ -30,17 +62,81 @@ function sla() {
                         ${week_days.map(week_day => `<div class="week_day">${week_day}</div>`).join(" ")}
                     </div>
                     <div class="month_days_conteiner">
-                        ${getDayNumbers(month).map(month_day => `<div class="month_day">
-                            <span class="circle">${month_day}</span>
-                        </div>`).join("")}
+                        ${getDayNumbers(month).map(month_day => `
+                            ${ month_day !== ""
+                                ? `<div class="month_day">
+                                        <span class="circle" id="${month}${month_day}" onclick="colorDay('${month}', ${month_day})">${month_day}</span>
+                                    </div>`
+                                : `<div class="month_day"></div>`
+                            }
+                        `).join("")}
                     </div>
                 </div>`).join("")}
             </div>
         </div>
     `
 
-    teste.innerHTML = newInnerHTML
+    content.innerHTML = newInnerHTML
+
+    let mood_btt1 = document.getElementById("mood1")
+    let mood_btt2 = document.getElementById("mood2")
+    let mood_btt3 = document.getElementById("mood3")
+    let mood_btt4 = document.getElementById("mood4")
+    let mood_btt5 = document.getElementById("mood5")
+    
+    mood_btt1.addEventListener("click", () => {
+        activeMood = mood_btt1.textContent
+        mood_btt1.className = "mood active"
+        mood_btt2.className = "mood"
+        mood_btt3.className = "mood"
+        mood_btt4.className = "mood"
+        mood_btt5.className = "mood"
+    })
+
+    mood_btt2.addEventListener("click", () => {
+        activeMood = mood_btt2.textContent
+        mood_btt1.className = "mood"
+        mood_btt2.className = "mood active"
+        mood_btt3.className = "mood"
+        mood_btt4.className = "mood"
+        mood_btt5.className = "mood"
+    })
+    
+    mood_btt3.addEventListener("click", () => {
+        activeMood = mood_btt3.textContent
+        mood_btt1.className = "mood"
+        mood_btt2.className = "mood"
+        mood_btt3.className = "mood active"
+        mood_btt4.className = "mood"
+        mood_btt5.className = "mood"
+    })
+    
+    mood_btt4.addEventListener("click", () => {
+        activeMood = mood_btt4.textContent
+        mood_btt1.className = "mood"
+        mood_btt2.className = "mood"
+        mood_btt3.className = "mood"
+        mood_btt4.className = "mood active"
+        mood_btt5.className = "mood"
+    })
+    
+    mood_btt5.addEventListener("click", () => {
+        activeMood = mood_btt5.textContent
+        mood_btt1.className = "mood"
+        mood_btt2.className = "mood"
+        mood_btt3.className = "mood"
+        mood_btt4.className = "mood"
+        mood_btt5.className = "mood active"
+    })
 }
 
+function colorDay(month, month_day) {
+    let num = document.getElementById(`${month}${month_day}`)
+    
+    num.style.backgroundColor = getComputedStyle(num).getPropertyValue(`--mood-color-${moods.indexOf(activeMood) + 1}`)
+}
 
-sla()
+const moods = ["Excelente", "Bom", "Mais ou menos", "Ruim", "Péssimo"]
+let activeMood = ""
+
+createCalendar()
