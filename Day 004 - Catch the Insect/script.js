92c109ee64907b478e74f313778a398686a0151a
Carlos <cs515164@gmail.com>
2020-09-06 21:42:49 -0300

    Project day 004 incomplete

---
+++ Day 004 - Catch the Insect/script.js
@@ -0,0 +1,9 @@
+let initial_page = document.getElementById("initialPage")
+let insect_selection = document.getElementById("insectSelection")
+
+insect_selection.style.display = "none"
+
+function insectPage() {
+    initial_page.style.display = "none"
+    insect_selection.style.display = "flex"
+}
