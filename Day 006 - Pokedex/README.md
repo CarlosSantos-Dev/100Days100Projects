18e834a786cb76d09318eefc07a5007b492590d6
Carlos Eduardo de Carvalho Santos <carlossantoscomercialctt@gmail.com>
2021-02-28 16:16:50 -0300

    Day 006's project final upload and README

---
+++ Day 006 - Pokedex/README.md
@@ -0,0 +1,29 @@
+# Day 006 - Pokedex
+
+## Tecnologias usadas
+Este projeto foi bem simples em relação às tecnologias usadas:
+
+- HTML;
+- CSS;
+- JavaScript;
+- [API que fornece dados dos pokemon](https://pokeapi.co/api/v2/pokemon/1). (O último número na URL é o que define o pokemon a ser mostrado.)
+
+## Observação
+Bem, eu havia parado de fazer estes desafios porque decidi focar em outra coisa, mas resolvi voltar depois de quase 6 meses.
+
+Vou colocar a data de início e término nos projetos a partir de agora.
+
+## Aprendizado
+Como eu havia começado este projeto há meses atrás, eu não me lembro bem dos desafios que passei, mas me conhecendo bem, eu sei que eu dei umas olhadinas no [código do FlorinPop](https://codepen.io/FlorinPop17/pen/gOYZxyE). 😔
+
+Isso porque tem código que não conhecia quando abri o script depois de todo esse tempo.
+
+Quando finalizei o código, eu comentei ele todo, então você pode ver o que eu aprendi, de fato, lá mesmo.
+
+## Finalização
+Essa desafio foi mediano, nem tão fácil, nem tão difícil.
+
+Até mais!
+
+Data de início: ?
+Data de término: 28/20/2021
--- Day 006 - Pokedex/script.js
+++ Day 006 - Pokedex/script.js
@@ -1,6 +1,6 @@
-const pokeContainer = document.getElementById("pokeContainer")
-const maxPokemon = 80
-const colors = {
+const pokeContainer = document.getElementById("pokeContainer") // Pega o elemento HTML a ser usado para mostrar os cards dos pokemon.
+const maxPokemon = 150 // O número máximo de pokemon a ser mostrado.
+const colors = { // Cores dos tipos de pokemon.
     normal: "#F5F5F5",
     fighting: "#E6E0D4",
     flying: "#F5F5F5",
@@ -8,41 +8,49 @@ const colors = {
     fairy: "#FCEAFF",
     fire: "#FDCDCD",
     water: "#c0eafd",
-    ground: "#665c53",
+    ground: "#F4E7DA",
+    rock: "#665c53",
     grass: "#81fc89",
     poison: "#8eca9b",
     electric: "#e9d882",
     psychic: "#EAEDA1"
 }
-let mainTypes = Object.keys(colors)
+let mainTypes = Object.keys(colors) // Pega cada chave da constante "colors" e cria um array que as armazena.
 
 const getPokemon = async id => {
-    let url = `https://pokeapi.co/api/v2/pokemon/${id}`
-    let res = await fetch(url)
-    let pokemon = await res.json()
-    createCard(pokemon)
+    let url = `https://pokeapi.co/api/v2/pokemon/${id}` // Guarda o url com dados do pokemon passado.
+    let res = await fetch(url) // Faz a resolução da promise.
+    let pokemon = await res.json() // Transforma os dados da promise para o formato JSON.
+    createCard(pokemon) // Envia os dados do pokemon para a função de criação do card.
 }
 
 function createCard(pokemon) {
-    let bgColor
-    let pokeNum
+    let bgColor // Guarda uma cor dependendo do tipo do pokemon.
+    let pokeNum // Guarda o id do pokemon.
 
     mainTypes.map(type => {
-        if(type === pokemon.types[0].type.name) {
-            bgColor = colors[`${type}`]
+        if(type === pokemon.types[0].type.name) { // Se o tipo dos pokemon armazenado anteriormente no array for igual ao tipo do pokemon atual...
+            bgColor = colors[`${type}`] // ...a cor do card vai ser a cor correspondente na constante de acordo com o tipo do pokemon.
         }
     })
 
-    if(pokemon.id < 10) {
-        pokeNum = `00${pokemon.id}`
-    } else if(pokemon.id < 100) {
-        pokeNum = `0${pokemon.id}`
-    } else {
-        pokeNum = pokemon.id
+    if(pokemon.id < 10) { // Se o id do pokemon for menor que 10...
+        pokeNum = `00${pokemon.id}` // ...põe dois zeros antes do id.
+    } else if(pokemon.id < 100) { // Se o id do pokemon for menor que 100...
+        pokeNum = `0${pokemon.id}` // ...põe um zero antes do id.
+    } else { // Senão...
+        pokeNum = pokemon.id // ...deixa somente o id do pokemon.
     }
 
+    let numType = 0 // Variável para armazenar qual tipo vai ser mostrado.
 
-    // console.log(pokemon.types[0].type.name === "normal" && !pokemon.types.length !== 1 ? pokemon.types[0].type.name : pokemon.types[1].type.name)
+    for(slot in pokemon.types) // Verifica cada tipo do pokemon a ser mostrado.
+    {
+        if(pokemon.types[slot].type.name === "normal" && pokemon.types.length === 2) // Se o primeiro tipo for normal e o tamanho do array de tipos for igual a 2...
+        {
+            numType = Number(slot+1) // ...o tipo do pokemon vai ser o segundo item do array.
+        }
+    }
 
     let newCard = `
         <div class="pokemon" style="background-color: ${bgColor};">
@@ -50,7 +58,7 @@ function createCard(pokemon) {
             <div class="info">
                 <span class="number">#${pokeNum}</span>
                 <h3 class="name">${pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h3>
-                <small class="type">Type: ${pokemon.types[0].type.name}</small>
+                <small class="type">Type: ${pokemon.types[numType].type.name}</small>
             </div>
         </div>
     `
@@ -58,9 +66,13 @@ function createCard(pokemon) {
     pokeContainer.innerHTML += newCard
 }
 
-async function fetchPokemon() {
-    let pokeCount = parseInt(Math.random() * (maxPokemon - 20) + 20)
-    for(let i=1; i<=pokeCount; i++) {
+async function fetchPokemon()
+{
+    let pokeCount = parseInt(Math.random() * (maxPokemon - 20) + 20) // Aleatoriza a quantidade de pokemon a ser mostrado.
+
+    for(let i=1; i<=pokeCount; i++) // Passa por cada número gerado na aleatorização e envia ele como id dos pokemon para a função abaixo.
+
+    {
         await getPokemon(i)
     }
 }
